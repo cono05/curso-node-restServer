3e5b94c229a35847a90f270cd166965278afe18c
Cono <rodriguezcono7@gmail.com>
2021-06-10 19:44:33 -0300

    Se comienza implementacion de Put en usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -41,12 +41,23 @@ const usuariosPost = async (req, res) =>{
     });
 }
 
-const usuariosPut = (req, res) =>{
-    const id = req.params.id;
+const usuariosPut = async (req, res) =>{
+    const {id} = req.params; //este es .params porque viene en la linea de la url
+    const {password, google, correo, ...resto} = req.body // excluye psw, google y correo del resto del body
+
+    //TODO validar contra base de datos
+    if(password){
+        const salt = bcrypt.genSaltSync(5);
+        resto.password = bcrypt.hashSync(password, salt);
+    }
+    //findByIdAndUpdate busca por id, y actualiza todo lo que va en resto
+    const usuarioDB = await Usuario.findByIdAndUpdate(id, resto);
     res.json({
         msg: 'put API desde controlador',
-        id
+        usuarioDB
     });
+
+
 }
 
 const usuariosDelete = (req, res) =>{
