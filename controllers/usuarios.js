58506889ff0b5d794c642fb15332ae4dcb7002f5
Cono <rodriguezcono7@gmail.com>
2021-06-10 22:23:36 -0300

    Implementacion de get con query y promesas ejecutadas en paralelo para mas performance

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -4,16 +4,33 @@ const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario')
 
-const usuariosGet = (req, res = response) =>{
+const usuariosGet = async (req, res = response) =>{
     //const query = req.query; con esta linea me quedo la query que viene en la url del get
-    const{ q, nombre = 'No name', apikey, page, limit} = req.query;
+    //const{ q, nombre = 'No name', apikey, page, limit} = req.query;
+    const{ limite = 5, desde = 0 } = req.query;
+    const query = {estado:true};
+    // const usuarios = await Usuario.find(query)
+    //     .skip(Number(desde))//se saltea los primeros "desde"
+    //     .limit(Number(limite));//es la cantidad de registros a recuperar
+
+    // const total = await Usuario.countDocuments(query);//esta linea es para contabilizar los registros, al ponerle estado:true
+    //solo va a contar los registros que cumplan con esa condicion. Esto nos puede servir en el obligatorio
+
+    //Una manera mas performante de hacer lo mismo es usando promise.all, dado que ejecuta las dos promesas al mismo tiempo
+    const [total , usuarios] = await Promise.all([ //total es el resultado de la primera promesa, usuarios es el resultado de la segunda promesa
+        Usuario.count(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ])
+
+
     res.json({
-        msg: 'get API desde controlador',
-        //query
-        q,
-        nombre,
-        apikey,
-        limit
+        total,
+        usuarios
+        //resp
+        // total,
+        // usuarios
     });
 }
 
@@ -52,10 +69,7 @@ const usuariosPut = async (req, res) =>{
     }
     //findByIdAndUpdate busca por id, y actualiza todo lo que va en resto
     const usuarioDB = await Usuario.findByIdAndUpdate(id, resto);
-    res.json({
-        msg: 'put API desde controlador',
-        usuarioDB
-    });
+    res.json(usuarioDB);
 
 
 }
